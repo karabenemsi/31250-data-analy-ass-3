dc09b9d58cbbc9c3f0e07412fbc7a7b6f6c20d68
KaraBeNemsi <karabenemsi@users.noreply.github.com>
2019-10-08 15:41:16 +1100

    Switch to pandas csv-reader

--- preprocess.py
+++ preprocess.py
@@ -1,134 +1,98 @@
-import csv
 from datetime import datetime
 import numpy as np
 import pandas as pd
 from sklearn import preprocessing
-from sklearn.feature_selection import VarianceThreshold
 
 pd.set_option('display.width', 0)
 
 
-def stringtobool(string):
+def string_to_bool(string):
     if string == 'Y':
-        return 1
+        return 1.0
     if string == 'N':
-        return 0
-    if string == '1':
-        return 1
-    if string == '0':
-        return 0
+        return 0.0
     # If its anything else return none
     return None
 
 
-def stringValue(string):
+def string_to_value(string):
     if string == '':
         return None
-
     if len(string) == 1:
         return ord(string) - 65
-
     value = 0
     for index, char in enumerate(string):
         value += (ord(char)-65) * pow(10, index)
     return value
 
+def format_amount(string):
+    return int(''.join(string.split(',')))
 
-rawData = []
 
-with open('TrainingSet.csv', 'r') as csvfile:
-    trainingSet = csv.DictReader(csvfile)
-    i = 0
-    for row in trainingSet:
-        # rawData.append(row)
 
-        if i < 20:
-            rawData.append(row)
-            i += 1
+def str_to_timestamp(x):
+    return int(datetime.strptime(x.zfill(10), '%d/%m/%Y').timestamp())
 
-print(len(rawData))
-# Convert Dates into python datetime and other basic parsing
-for row in rawData:
-    row['Quote_ID'] = int(row['Quote_ID'])
-    row['Original_Quote_Date'] = int(datetime.strptime(row['Original_Quote_Date'].zfill(10), '%d/%m/%Y').timestamp())
-    row['QuoteConversion_Flag'] = stringtobool(row['QuoteConversion_Flag'])
-    row['Field_info1'] = stringValue(row['Field_info1'])
-    row['Field_info2'] = float(row['Field_info2']) if row['Field_info2'] != '' else None
-    # Remove thousand indicator
-    row['Field_info3'] = row['Field_info3'].replace(',', '')
-    row['Field_info3'] = int(row['Field_info3']) if row['Field_info3'] != '' else None
-    row['Field_info4'] = stringtobool(row['Field_info4'])
 
-    row['Coverage_info1'] = int(row['Coverage_info1']) if row['Coverage_info1'] != '' else None
-    row['Coverage_info2'] = int(row['Coverage_info2']) if row['Coverage_info2'] != '' else None
-    row['Coverage_info3'] = stringValue(row['Coverage_info3'])
+data = pd.read_csv('TrainingSet.csv')
+print(data.shape)
 
-    row['Sales_info1'] = stringtobool(row['Sales_info1'])
-    row['Sales_info2'] = int(row['Sales_info2']) if row['Sales_info2'] != '' else None
-    row['Sales_info3'] = int(row['Sales_info2']) if row['Sales_info2'] != '' else None
-    row['Sales_info4'] = stringValue(row['Sales_info4'])
-    row['Sales_info5'] = int(row['Sales_info5']) if row['Sales_info5'] != '' else None
 
-    row['Personal_info1'] = stringtobool(row['Personal_info1'])
-    row['Personal_info2'] = int(row['Personal_info2']) if row['Personal_info2'] != '' else None
-    row['Personal_info3'] = stringValue(row['Personal_info3'])
-    row['Personal_info4'] = stringtobool(row['Personal_info4'])
-    row['Personal_info5'] = int(row['Personal_info5']) if row['Personal_info5'] != '' else None
+# Convert Date
+data['Original_Quote_Date'] = data['Original_Quote_Date'].apply(str_to_timestamp)
 
-    row['Property_info1'] = stringtobool(row['Property_info1'])
-    row['Property_info2'] = stringtobool(row['Property_info2'])
-    row['Property_info3'] = stringValue(row['Property_info3'])
-    row['Property_info4'] = stringtobool(row['Property_info4'])
-    row['Property_info5'] = int(row['Property_info5']) if row['Property_info5'] != '' else None
+# Convert bool-values to int of 1 and 0
+data['Field_info4'] = data['Field_info4'].apply(string_to_bool)
+data['Personal_info1'] = data['Personal_info1'].apply(string_to_bool)
+data['Property_info1'] = data['Property_info1'].apply(string_to_bool)
+data['Geographic_info4'] = data['Geographic_info4'].apply(string_to_bool)
 
-    row['Geographic_info1'] = int(row['Geographic_info1']) if row['Geographic_info1'] != '' else None
-    row['Geographic_info2'] = int(row['Geographic_info2']) if row['Geographic_info2'] != '' else None
-    row['Geographic_info3'] = int(row['Geographic_info3']) if row['Geographic_info3'] != '' else None
-    row['Geographic_info4'] = stringtobool(row['Geographic_info4'])
-    # row['Geographic_info5'] = stringValue(row['Geographic_info5'])
+# Convert string to int values
+data['Field_info1'] = data['Field_info1'].apply(string_to_value)
+data['Coverage_info3'] = data['Coverage_info3'].apply(string_to_value)
+data['Sales_info4'] = data['Sales_info4'].apply(string_to_value)
+data['Personal_info3'] = data['Personal_info3'].apply(string_to_value)
+data['Property_info3'] = data['Property_info3'].apply(string_to_value)
+
+#Convert special amount to int
+data['Field_info3'] = data['Field_info3'].apply(format_amount)
 
 # Find empty values
-count = dict()
-countNone = dict()
-for row in rawData:
-    if -1 in row.values():
-        for item in row.items():
-            if item[1] is -1:
-                count[item[0]] = count[item[0]] + 1 if item[0] in count else 1
-    if None in row.values():
-        for item in row.items():
-            if item[1] is None:
-                countNone[item[0]] = countNone[item[0]] + 1 if item[0] in countNone else 1
+print(data.isnull().sum())
 
-print('-1 in: ' + str(count))
-print('None in: ' + str(countNone))
+# Feature Pre-Selection
 
-data = pd.DataFrame(rawData)
-# print(data.isnull().sum())
-
-# Feature Selection
-
+keepColumns = ['QuoteConversion_Flag']
 # Drop Personal_Info5 and Geographic_info3 as most of it is empty anyway
 data.drop(columns=['Personal_info5', 'Geographic_info3'], inplace=True)
 # Remove Rows with empty values
 data.dropna(inplace=True)
 
+# Change Categorical to num
+dummies = dict()
+dummies['Geographic_info5'] = pd.get_dummies(data['Geographic_info5'])
+for dum in dummies:
+    # Keep generated columns as they might include lots of empty(same) values
+    keepColumns = keepColumns + list(dummies[dum].keys())
+    data.drop(columns=[dum], inplace=True)
+    data = pd.concat([data, dummies[dum]], axis=1)
+
+print(data.describe())
+
 # Remove rows with low variance
 remove = []
+print(keepColumns)
 for col in data:
-    var = data.loc[:, col].var()
-    # If variance is really low remember for removal it (except the Quote flag)
-    if var < (.8 * (1 - .8))and col != 'QuoteConversion_Flag':
-        remove.append(col)
-        print('Remove ' + col + ' with variance of ' + str(var))
+    if col not in keepColumns:
+        print(col)
+        var = data.loc[:, col].var()
+        # If variance is really low remember for removal
+        if var < (.8 * (1 - .8)):
+            remove.append(col)
+            print('Remove ' + col + ' with variance of ' + str(var))
 
 # Drop all rows with low variance
-data.drop(columns=remove, inplace=True)
-
-
-
-
-
+# data.drop(columns=remove, inplace=True)
 
 print(data.shape)
 
