7e6e27957ae18c20fb5cacd0cd1a2891af918fd7
KaraBeNemsi <karabenemsi@users.noreply.github.com>
2019-10-11 09:38:17 +1100

    Better handling of unsure rows

--- main.py
+++ main.py
@@ -12,154 +12,148 @@ from sklearn.svm import LinearSVC
 from sklearn.naive_bayes import MultinomialNB
 from imblearn.over_sampling import SMOTE
 from sklearn.metrics import roc_auc_score
-from sklearn.model_selection import GridSearchCV
 
 
-def kFoldModel(model, X, y):
+def k_fold_model(classifier_model, x, y):
     cv = StratifiedKFold(n_splits=5)
-    for train_idx, test_idx, in cv.split(X, y):
-        X_train, y_train = X[train_idx], y[train_idx]
-        X_test, y_test = X[test_idx], y[test_idx]
+    for train_idx, test_idx, in cv.split(x, y):
+        x_train, y_train = x[train_idx], y[train_idx]
+        x_test, y_test = x[test_idx], y[test_idx]
 
         # Use SMOTE to oversample the dataset for better training accuracy
         sm = SMOTE()
-        X_train_oversampled, y_train_oversampled = sm.fit_sample(X_train, y_train)
+        x_train_oversampled, y_train_oversampled = sm.fit_sample(x_train, y_train)
 
         # Fit and predict
-        model.fit(X_train_oversampled, y_train_oversampled)
-        y_pred = model.predict(X_test)
+        classifier_model.fit(x_train_oversampled, y_train_oversampled)
+        y_pred = classifier_model.predict(x_test)
 
         print(f'auc: {roc_auc_score(y_test, y_pred)}')
-    return model
+    return classifier_model
 
 
+def decide_for_unsure(df, name):
+    df['Sum'] = df.sum(axis=1)
+    final = []
+    width = len(df.keys()) - 1
+    count = []
+    # init counter
+    for i in range(0, width + 1):
+        count.append(0)
+    for row in df['Sum']:
+        if 0 < row < width:
+            # Do +1 as most of the data points tend to be 0 rather than 1
+            final.append(1 if row > ((width / 2) + 1) else 0)
+            count[row] += 1
+        else:
+            count[row] += 1
+            final.append(0 if row == 0 else 1)
+    df['Final'] = final
+    for i in range(0, width + 1):
+        print(str(count[i]) + ' times ' + str(i) + ' on ' + name)
+    print(str(sum(count[1:-1]) / len(df) * 100) + '% Unsure on ' + name)
+    return df
 
-## Get Preprocessed Data
+
+# Get Preprocessed Data
 train_target, train_data, test_data, train_df, test_df = preprocess.preprocess(
     "TrainingSet.csv", 'TestSet.csv', limit=None, remove_low_variance=True, remove_outliers=True)
 X_g_train, X_g_test, y_g_train, y_g_test = train_test_split(train_data, train_target, test_size=0.30)
-print(f'Trainset has {train_target.sum()} times 1')
+print(f'TrainSet has {train_target.sum()} times 1')
 
-## Init some variables for later use
+# Init some variables for later use
 result_predict = dict()
 test_predict = dict()
 
-
-## Random Forest
+# Random Forest
 print('Start Random Forest')
 model = RandomForestClassifier(n_estimators=128, criterion='entropy', n_jobs=-1)
-model = kFoldModel(model, train_data, train_target)
+model = k_fold_model(model, train_data, train_target)
 y_predict = model.predict(X_g_test)
 print(roc_auc_score(y_g_test, y_predict))
 
 result_predict['RandomForest'] = np.array(model.predict(test_data))
 test_predict['RandomForest'] = np.array(model.predict(X_g_test))
 
-## k-nearest neighbour
+# k-nearest neighbour
 print('Start k-nearest neighbour')
 model = KNeighborsClassifier(n_neighbors=20, weights='uniform', n_jobs=-1)
-model = kFoldModel(model, train_data, train_target)
+model = k_fold_model(model, train_data, train_target)
 y_predict = model.predict(X_g_test)
 print(roc_auc_score(y_g_test, y_predict))
 
 result_predict['KNeighbors'] = np.array(model.predict(test_data))
 test_predict['KNeighbors'] = np.array(model.predict(X_g_test))
 
-## SVM
+# SVM
 print('SVM')
 model = SVC(gamma='auto', kernel='rbf')
-model = kFoldModel(model, train_data, train_target)
+model = k_fold_model(model, train_data, train_target)
 y_predict = model.predict(X_g_test)
 print(roc_auc_score(y_g_test, y_predict))
 
 result_predict['SVM'] = np.array(model.predict(test_data))
 test_predict['SVM'] = np.array(model.predict(X_g_test))
 
-## Linear SVM
+# Linear SVM
 print('Linear SVM')
 model = LinearSVC(random_state=0, tol=1e-5, dual=True, loss='squared_hinge')
-model = kFoldModel(model, train_data, train_target)
+model = k_fold_model(model, train_data, train_target)
 y_predict = model.predict(X_g_test)
 print(roc_auc_score(y_g_test, y_predict))
 
 result_predict['LinearSVM'] = np.array(model.predict(test_data))
 test_predict['LinearSVM'] = np.array(model.predict(X_g_test))
 
-## Naive Bayes
-#print('Naive Bayes')
-#model = MultinomialNB()
-#model = kFoldModel(model, train_data, train_target)
-#y_predict = model.predict(X_g_test)
-#print(roc_auc_score(y_g_test, y_predict))
+# Naive Bayes
+# print('Naive Bayes')
+# model = MultinomialNB()
+# model = kFoldModel(model, train_data, train_target)
+# y_predict = model.predict(X_g_test)
+# print(roc_auc_score(y_g_test, y_predict))
 
-#result_predict['NaiveBayes'] = np.array(model.predict(test_data))
-#test_predict['NaiveBayes'] = np.array(model.predict(X_g_test))
+# result_predict['NaiveBayes'] = np.array(model.predict(test_data))
+# test_predict['NaiveBayes'] = np.array(model.predict(X_g_test))
 
-## SVM
-#print('Radius Neighbors')
-#model = RadiusNeighborsClassifier(radius=1.0)
-#model = kFoldModel(model, train_data, train_target)
-#y_predict = model.predict(X_g_test)
-#print(roc_auc_score(y_g_test, y_predict))
+# SVM
+# print('Radius Neighbors')
+# model = RadiusNeighborsClassifier(radius=1.0)
+# model = kFoldModel(model, train_data, train_target)
+# y_predict = model.predict(X_g_test)
+# print(roc_auc_score(y_g_test, y_predict))
 
 result_predict['radiusNeighbors'] = np.array(model.predict(test_data))
 test_predict['radiusNeighbors'] = np.array(model.predict(X_g_test))
 
-
-
-## Neural Network
+# Neural Network
 print('Start MLPClassifier')
 model = MLPClassifier(solver='adam', alpha=0.0001, learning_rate_init=0.001,
                       hidden_layer_sizes=(24, 23, 22), max_iter=1000)
-model = kFoldModel(model, train_data, train_target)
+model = k_fold_model(model, train_data, train_target)
 y_predict = model.predict(X_g_test)
 print(roc_auc_score(y_g_test, y_predict))
 
 result_predict['mlpNetwork'] = np.array(model.predict(test_data))
 test_predict['mlpNetwork'] = np.array(model.predict(X_g_test))
 
-## Do stuff with unsure rows
+# Do stuff with unsure rows
 
 result_df = pd.DataFrame(result_predict)
-result_df['Sum'] = result_df.sum(axis=1)
+result_df = decide_for_unsure(result_df, 'TestSet')
 
-count = 0
-final = []
-width = len(result_df.keys()) - 1
-for row in result_df['Sum']:
-    if 0 < row < width:
-        final.append(1 if row >= (width / 2) else 0)
-        count += 1
-    else:
-        final.append(0 if row == 0 else 1)
-result_df['Final'] = final
-print(str(count / len(result_df) * 100) + '% Unsure on TestSet')
-
+print('/n Test:')
 # Do it for test to
 t_df = pd.DataFrame(test_predict)
-t_df['Sum'] = t_df.sum(axis=1)
+t_df = pd.DataFrame(test_predict)
+t_df = decide_for_unsure(result_df, 'TestSet')
 
-count = 0
-final = []
-width = len(t_df.keys()) - 1
-for row in t_df['Sum']:
-    if 0 < row < width:
-        final.append(1 if row >= (width / 2) else 0)
-        count += 1
-    else:
-        final.append(0 if row == 0 else 1)
-t_df['Final'] = final
-print(str(count / len(t_df) * 100) + '% Unsure on TrainTest')
-
-print(f'AUC: {roc_auc_score(y_g_test, list(t_df["Final"]))}')
-
-### Save Result to file
+# Save Result to file
 test_df['QuoteConversion_Flag'] = pd.Series(result_df['Final'], index=test_df.index)
 
-todrop = []
+toDrop = []
 for col in test_df.columns:
     if col not in ['Quote_ID', 'QuoteConversion_Flag']:
-        todrop.append(col)
-test_df.drop(columns=todrop, inplace=True)
+        toDrop.append(col)
+test_df.drop(columns=toDrop, inplace=True)
 test_df.to_csv('Kaggle_Submission.csv', index=False)
 print('Written to file')
